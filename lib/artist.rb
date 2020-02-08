266849d43ab7f66a44e9ca97878ffaac35152d85
Khalid T <khalid.tissire@gmail.com>
2020-02-08 05:29:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,18 +20,18 @@ class Artist
   end
 
   def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select { |song| song.artist === self }
+  end
+  
+  def self.find(name)
+    self.all.find {|artist| artist.name === name }
   end
 
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.new(name)
   end
 
-  def self.find(name)
-    self.all.find {|artist| artist.name == name }
-  end
-
   def print_songs
-    songs.each {|song| puts song.name}
+    songs.each { |s| puts s.name }
   end
 end
